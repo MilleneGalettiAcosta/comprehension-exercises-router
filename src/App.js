f2760d287db2c7c1f6203df48935182394d4ca1b
Millene Galetti <mikagaletti1@gmail.com>
2021-11-10 20:39:18 -0300

    Exercicio 10

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ class App extends Component {
       </ul>
     </nav>
     <Switch>
-      <Route exact path="/" component={Home}/>
+    <Route exact path="/users/:id" render={ (props) => (<Users {...props} greetingMessage={"Good Morning"} />)} />
       <Route exact path="/about" component={About} />
-      <Route exact path="/users/:id" render={ (props) => (<Users {...props} greetingMessage={"Good Morning"} />)} />
+      <Route exact path="/" component={Home}/>
     </Switch>
     </BrowserRouter>
     );
